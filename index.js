e12a232017a83225e11342b4d60e34b79a283081
eselbeus <eselbeus@gmail.com>
2018-10-01 06:09:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {
+  prop: 1
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObject = Object.assign({}, object, {[key]: value});
+  return newObject;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function 
